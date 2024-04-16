4b0858c50b8b0547b3659c91f53f84d761ecc6ed
Guillaume Andreux <guillaume.andreux@nvo.fr>
2024-04-16 15:18:06 +0200

    add ORM sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,23 @@ import { AppRoutingModule } from './app.routing-module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
     imports: [AppRoutingModule, ConfigurationModule, DatabaseModule],
 })
+
+@Module({
+    imports: [
+      SequelizeModule.forRoot({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'test',
+        models: [],
+      }),
+    ],
+  })
 export class AppModule {}
