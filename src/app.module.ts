91af29d7f25bf9ad4fff61e20b1c0a27203208b0
Guillaume Andreux <guillaume.andreux@nvo.fr>
2024-04-18 11:37:52 +0200

    config sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,22 @@ import { ConfigurationModule } from './infrastructure/configuration/configuratio
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      autoLoadModels: true,
-      synchronize: true,
-    }),
-    AppRoutingModule, 
-    ConfigurationModule, 
-    DatabaseModule,
-    UserModule,
-  ],
+    imports: [AppRoutingModule, ConfigurationModule, DatabaseModule],
 })
-
+@Module({
+        imports: [
+            SequelizeModule.forRoot({
+                dialect: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'root',
+                password: 'root',
+                database: 'test',
+                autoLoadModels: true,
+                synchronize: true,
+            }),
+        ],
+})
 export class AppModule {}
