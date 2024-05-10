3ed5f0d4793cfa0a2ef8770961791f4fe521f690
Yome07 <guillaume@andreux.fr>
2024-05-10 14:43:40 +0200

    add post method

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller()
@@ -9,5 +9,14 @@ export class TaskController {
     getUserTasks(@Param('userId') userId: string) {
         return this.taskService.getUserTasks(userId);
     }
+
+    @Post()
+    async createTask(
+        @Body('name') name: string,
+        @Body('userId') userId: number,
+        @Body('priority') priority: number,
+    ) {
+        await this.taskService.addTask(name, userId, priority);
+    }  
 
 }
