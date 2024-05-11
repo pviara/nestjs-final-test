5579b3422591d70431f8f9c4bf8ee22ae1bf5c81
Yome07 <guillaume@andreux.fr>
2024-05-11 16:04:32 +0200

    add post to task controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Controller()
 export class TaskController {
@@ -11,12 +12,10 @@ export class TaskController {
     }
 
     @Post()
-    async createTask(
-        @Body('name') name: string,
-        @Body('userId') userId: number,
-        @Body('priority') priority: number,
+    @UsePipes(new ValidationPipe())
+    async createTask(@Body() CreateTaskDTO: CreateTaskDTO 
     ) {
-        await this.taskService.addTask(name, userId, priority);
+        await this.taskService.addTask(CreateTaskDTO.name, CreateTaskDTO.userId, CreateTaskDTO.priority);
     }  
 
 }
